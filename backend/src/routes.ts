7b5244a67133f21f9b922cabf35b248ba99dcde0
Henry Romero <125617803+officialhenryromero@users.noreply.github.com>
2024-06-04 21:05:19 -0400

    Added comments to router

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -14,18 +14,18 @@ export const Routes = (router: Router) => {
     router.get('/api/me', AuthMiddleware, AuthController.Me);
     router.put('/api/me/update', AuthMiddleware, AuthController.UpdateInfo);
     router.put('/api/me/updatePassword', AuthMiddleware, AuthController.UpdatePassword);
-
+    // I also added roles because it's a common practice to have a role-based access control
     router.get('/api/roles', AuthMiddleware, PermissionsMiddleware(AppPermissions.READ_ROLE), RoleController.List);
     router.get('/api/roles/:id', AuthMiddleware, PermissionsMiddleware(AppPermissions.READ_ROLE), RoleController.Get);
     router.post('/api/roles', AuthMiddleware, PermissionsMiddleware(AppPermissions.CREATE_ROLE), RoleController.Create);
     router.put('/api/roles/:id', AuthMiddleware, PermissionsMiddleware(AppPermissions.UPDATE_ROLE), RoleController.Update);
     router.delete('/api/roles/:id', AuthMiddleware, PermissionsMiddleware(AppPermissions.DELETE_ROLE), RoleController.Delete);
-
+    // This is what we're here for
     router.get('/api/tasks', AuthMiddleware, PermissionsMiddleware(AppPermissions.READ_TASK), TaskController.List);
     router.get('/api/tasks/:id', AuthMiddleware, PermissionsMiddleware(AppPermissions.READ_TASK), TaskController.Get);
     router.post('/api/tasks', AuthMiddleware, PermissionsMiddleware(AppPermissions.CREATE_TASK), TaskController.Create);
     router.put('/api/tasks/:id', AuthMiddleware, PermissionsMiddleware(AppPermissions.UPDATE_TASK), TaskController.Update);
     router.delete('/api/tasks/:id', AuthMiddleware, PermissionsMiddleware(AppPermissions.DELETE_TASK), TaskController.Delete);
-
+    // I also added task statused because ... why not?
     router.get('/api/task-statuses', AuthMiddleware, PermissionsMiddleware(AppPermissions.READ_TASK_STATUS), TaskController.ListStatuses);
 };
